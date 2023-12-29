1ac0fcec5b3673483e6b3588c4aad3c15e7191a1
Divya Thakur <divya.thakur@edurev.in>
2023-12-29 16:46:06 +0530

    mvvm list and adapter

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -28,7 +28,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
          binding.rvList.layoutManager = LinearLayoutManager(this)
